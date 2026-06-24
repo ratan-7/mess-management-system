a59fd1947c7fd8489d49f046d217a3cdedfc4553
Ratan Mahata <ratanmahata116@gmail.com>
2026-06-24 20:15:37 +0530

    authController working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const connectDB = require("./config/db")
+const authRoutes = require("./routes/authRoutes")
+
+
 const app = express();
 const port = 3000;
 
 app.use(express.json())
 connectDB();
+app.use("/api", authRoutes)
+
 app.get('/', (req, res) => {
     res.send("hello world");
 });
